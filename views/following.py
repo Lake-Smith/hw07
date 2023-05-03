471695a3f6ba5294921f68b49fb2f50b94e02b24
Lake_Smith <lsmith17@unca.edu>
2023-05-02 22:54:51 -0400

    final push

--- views/following.py
+++ views/following.py
@@ -3,6 +3,7 @@ from flask_restful import Resource
 from models import Following, User, db
 import access_utils
 import json
+import flask_jwt_extended
 
 def get_path():
     return request.host_url + 'api/posts/'
@@ -11,12 +12,14 @@ class FollowingListEndpoint(Resource):
     def __init__(self, current_user):
         self.current_user = current_user
     
+    @flask_jwt_extended.jwt_required()
     def get(self):
         # return all of the "following" records that the current user is following
         following = Following.query.filter_by(user_id=self.current_user.id).all()
         #print(followers.to_dict())
         return Response(json.dumps([follow.to_dict_following() for follow in following]), mimetype="application/json", status=200)
 
+    @flask_jwt_extended.jwt_required()
     def post(self):
         body = request.get_json()
         #(body)
@@ -47,6 +50,7 @@ class FollowingDetailEndpoint(Resource):
     def __init__(self, current_user):
         self.current_user = current_user
     
+    @flask_jwt_extended.jwt_required()
     def delete(self, id):
          # delete "bookmark" record where "id"=id
         try:
@@ -70,11 +74,11 @@ def initialize_routes(api):
         FollowingListEndpoint, 
         '/api/following', 
         '/api/following/', 
-        resource_class_kwargs={'current_user': api.app.current_user}
+        resource_class_kwargs={'current_user': flask_jwt_extended.current_user}
     )
     api.add_resource(
         FollowingDetailEndpoint, 
         '/api/following/<int:id>', 
         '/api/following/<int:id>/', 
-        resource_class_kwargs={'current_user': api.app.current_user}
+        resource_class_kwargs={'current_user': flask_jwt_extended.current_user}
     )
--- views/posts.py
+++ views/posts.py
@@ -3,8 +3,8 @@ from flask_restful import Resource
 from models import Post, Following, db
 from views import get_authorized_user_ids
 import access_utils
-
 import json
+import flask_jwt_extended
 
 def get_path():
     return request.host_url + 'api/posts/'
@@ -14,6 +14,7 @@ class PostListEndpoint(Resource):
     def __init__(self, current_user):
         self.current_user = current_user
 
+    @flask_jwt_extended.jwt_required()
     def get(self):
         me_and_my_friend_ids = access_utils.get_list_of_user_ids_in_my_network(self.current_user.id)
         try:
@@ -30,6 +31,7 @@ class PostListEndpoint(Resource):
         posts = Post.query.filter(Post.user_id.in_(me_and_my_friend_ids)).limit(limit)
         return Response(json.dumps([post.to_dict() for post in posts]), mimetype="application/json", status=200)
 
+    @flask_jwt_extended.jwt_required()
     def post(self):
         # request.get_json() is holding the data that the user
         # just sent over the network. Stored as a python dictionary
@@ -55,7 +57,7 @@ class PostDetailEndpoint(Resource):
     def __init__(self, current_user):
         self.current_user = current_user
         
-
+    @flask_jwt_extended.jwt_required()
     @access_utils.can_modify_or_404
     def patch(self, id):
         '''
@@ -83,13 +85,14 @@ class PostDetailEndpoint(Resource):
 
         return Response(json.dumps(post.to_dict()), mimetype="application/json", status=200)
 
-
+    @flask_jwt_extended.jwt_required()
     @access_utils.can_modify_or_404
     def delete(self, id):
         Post.query.filter_by(id=id).delete()
         db.session.commit()
         return Response(json.dumps(None), mimetype="application/json", status=200)
 
+    @flask_jwt_extended.jwt_required()
     @access_utils.can_view_or_404
     def get(self, id):
         # get the post based on the id
@@ -101,10 +104,10 @@ def initialize_routes(api):
     api.add_resource(
         PostListEndpoint, 
         '/api/posts', '/api/posts/', 
-        resource_class_kwargs={'current_user': api.app.current_user}
+        resource_class_kwargs={'current_user': flask_jwt_extended.current_user}
     )
     api.add_resource(
         PostDetailEndpoint, 
         '/api/posts/<int:id>', '/api/posts/<int:id>/',
-        resource_class_kwargs={'current_user': api.app.current_user}
+        resource_class_kwargs={'current_user': flask_jwt_extended.current_user}
     )
